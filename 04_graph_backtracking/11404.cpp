0a35729ded0a18b6a74660cd41698ef75d1135cf
sohee52 <wneh2023@naver.com>
2026-01-20 00:35:47 +0900

    solve: 14938. 서강그라운드

--- 04_graph_backtracking/11404.cpp
+++ 04_graph_backtracking/11404.cpp
@@ -1,41 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
-const long long INF = 1e15;
+
+typedef long long ll;
+const ll INF = 1e18;
+const ll MAX_N = 104;
+
+ll dist[MAX_N][MAX_N], n, m, u, v, w;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int n, m;
     cin >> n >> m;
 
-    vector<vector<long long>> dist(n+1, vector<long long>(n+1, INF));
-
-    // 같은 정점은 비용 0
-    for(int i = 1; i <= n; i++) {
-        dist[i][i] = 0;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (i == j) dist[i][j] = 0;
+            else dist[i][j] = INF;
+        }
     }
 
-    // 버스 정보 입력 (같은 경로 여러 개 있을 수 있음 -> 최소값 저장)
-    for(int i = 0; i < m; i++) {
-        int a, b;
-        long long c;
-        cin >> a >> b >> c;
-        dist[a][b] = min(dist[a][b], c);
+    for (int i = 0; i < m; i++) {
+        cin >> u >> v >> w;
+        dist[u][v] = min(dist[u][v], w);
     }
 
-    // 플로이드-워셜
-    for(int k = 1; k <= n; k++) {
-        for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= n; j++) {
-                if(dist[i][k] + dist[k][j] < dist[i][j]) {
-                    dist[i][j] = dist[i][k] + dist[k][j];
+    for (int k = 1; k <= n; k++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (dist[i][k] != INF && dist[k][j] != INF) {
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
                 }
             }
         }
     }
 
-    // 출력: 갈 수 없으면 0
     for(int i = 1; i <= n; i++) {
         for(int j = 1; j <= n; j++) {
             if(dist[i][j] == INF) cout << 0;
